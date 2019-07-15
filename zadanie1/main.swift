ac5cf901f22424ee2a81e153df8dae65b495fa44
Egor Devyatov <edevyatov@mac-mini-denis-bogatyrev.local>
2019-07-15 15:56:46 +0400

    Initial Commit

---
+++ zadanie1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  zadanie1
+//
+//  Created by Egor Devyatov on 15.07.2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
